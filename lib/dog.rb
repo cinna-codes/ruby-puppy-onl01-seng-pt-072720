3baba06bdcbf892fd0aa5b5c2a00ebf60495c63b
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-14 21:01:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Add your code here
+
+class Dog
+
+attr_accessor :name
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |pup|
+      puts "#{pup}"
+    end
+  end
+
+end
